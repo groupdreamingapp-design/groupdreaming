88ba43865b89a7e5f8067a5dc603e2f36a2de11a
Juan Manuel Correa <group.dreaming.app@gmail.com>
2026-01-01 00:49:38 +0000

    recordar que al segundo vencimiento pasadas las 72 hs se subasta el plan

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,5 +1,4 @@
 
-
 import type { Group, User, Transaction, Auction, Installment, Award } from './types';
 import { PlaceHolderImages } from './placeholder-images';
 import { format, addMonths, setDate, addDays, parseISO } from 'date-fns';
@@ -23,7 +22,7 @@ const calculateCuotaPromedio = (capital: number, plazo: number): number => {
 }
 
 export const initialGroups: Group[] = [
-    { id: "ID-20240115-9998", capital: 15000, plazo: 48, cuotaPromedio: 345, membersCount: 96, totalMembers: 96, status: "Activo", monthsCompleted: 12, userIsMember: true, userIsAwarded: true, activationDate: '2024-01-15T12:00:00Z' },
+    { id: "ID-20240115-9998", capital: 15000, plazo: 48, cuotaPromedio: 345, membersCount: 96, totalMembers: 96, status: "Activo", monthsCompleted: 23, userIsMember: true, userIsAwarded: false, activationDate: '2024-01-15T12:00:00Z' },
     { id: "ID-20230720-9999", capital: 15000, plazo: 36, cuotaPromedio: 455, membersCount: 72, totalMembers: 72, status: "Cerrado", monthsCompleted: 36, userIsMember: true, userIsAwarded: true, activationDate: '2023-07-20T12:00:00Z' },
     { id: "ID-20240510-8888", capital: 20000, plazo: 60, cuotaPromedio: calculateCuotaPromedio(20000, 60), membersCount: 120, totalMembers: 120, status: "Activo", monthsCompleted: 1, userIsMember: true, userIsAwarded: false, activationDate: '2024-05-10T12:00:00Z' },
     { id: "ID-20231101-7777", capital: 10000, plazo: 24, cuotaPromedio: calculateCuotaPromedio(10000, 24), membersCount: 48, totalMembers: 48, status: "Activo", monthsCompleted: 2, userIsMember: true, userIsAwarded: false, activationDate: '2023-11-01T12:00:00Z' },
